612e40ff017606c45850458314e60d425a5f9055
Kreal11 <kirillshyrokov2@gmail.com>
2024-05-08 18:55:11 +0300

    feature: initialized projects in FE and BE
    + connected Mongo DB to backend

---
+++ apps/backend/src/app.ts
@@ -0,0 +1,45 @@
+import express, { NextFunction, Request, Response } from "express";
+import logger from "morgan";
+import cors from "cors";
+import "dotenv/config";
+// import boardsRouter from "./routes/api/boards";
+// import cardsRouter from "./routes/api/cards";
+
+interface CustomError extends Error {
+  status?: number;
+  message: string;
+}
+
+const app = express();
+
+const formatsLogger: string =
+  app.get("env") === "development" ? "dev" : "short";
+
+app.use((req, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// app.use("/boards", boardsRouter);
+// app.use("/cards", cardsRouter);
+
+app.use((req, res: Response) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err: CustomError, req: Request, res: Response) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message, err });
+});
+
+export default app;
+
+// ycqTVHFlSH0GHZpp
